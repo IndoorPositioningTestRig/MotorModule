cacd4b7378a4827bcf3ada2eb983abf97edf72b3
Mauro de Lyon <maurodelyon@gmail.com>
2017-11-28 14:47:05 +0100

    motor driver implementation

--- MotorModule/src/main.cpp
+++ MotorModule/src/main.cpp
@@ -1,5 +1,7 @@
 #include "Arduino.h"
-
+/*
+* State machine declaration
+*/
 typedef enum {
   LISTENING,
   SETTING_LENGTH,
@@ -27,19 +29,45 @@ StateMachineType StateMachine[] = {
 
 StateType SmState = LISTENING;
 
-int EncoderPinA = 11;
-int EncoderPinB = 12;
+/*
+* GPIO declaration
+*/
+// Motor driver
+int retractPin = 2;
+int motorDriverPwmPin = 3;
+int feedPin = 4;
 
+// End switches
+int switchAPin = 5;
+int switchBPin = 6;
+// Encoder
+int EncoderPinA = 7;
+int EncoderPinB = 8;
+
+// Global variables
 int EncoderLast = LOW;
 int EncoderCurrent = LOW;
 
 int EncoderPos = 0;
 int desiredPos = 0;
 
+int desiredSpeed = 0;
+int currentSpeed = 0;
+
+int acceleration = 0;
+
 String Command = "0";
 
 void setup()
 {
+  // Motor driver setup
+  pinMode(retractPin, OUTPUT);
+  pinMode(motorDriverPwmPin, OUTPUT);
+  pinMode(feedPin, OUTPUT);
+
+  // TODO Switch setup
+
+  // encoder setup
   pinMode(EncoderPinA, INPUT);
   pinMode(EncoderPinB, INPUT);
   Serial.begin(9600);
@@ -88,9 +116,16 @@ void Sm_SettingLength(void)
 {
   // Set correct length
   int length = Command.substring(Command.indexOf('|', 3) + 1, Command.indexOf('|', 4)).toInt();
+  int speed = Command.substring(Command.indexOf('|', 4) + 1, Command.length()).toInt();
   desiredPos = length;
-  Serial.print("Desired length: ");
-  Serial.println(desiredPos);
+  desiredSpeed = speed;
+  Serial.print("Current length: ");
+  Serial.print(EncoderPos);
+  Serial.print(" Desired length: ");
+  Serial.print(desiredPos);
+  Serial.print(" Speed: ");
+  Serial.println(desiredSpeed);
+
   SmState = DONE;
 }
 
@@ -102,7 +137,6 @@ void Sm_Moving(void)
   {
     if (digitalRead(EncoderPinB) == LOW)
     {
-
       EncoderPos--;
     }
     else
@@ -114,12 +148,36 @@ void Sm_Moving(void)
     Serial.print(" Desired length: ");
     Serial.println(desiredPos);
   }
-  if (desiredPos == EncoderPos)
+  EncoderLast = EncoderCurrent;
+
+  if (currentSpeed < desiredSpeed && acceleration > 500)
   {
+    currentSpeed++;
+    acceleration = 0;
+  }
+  else
+  {
+    acceleration++;
+  }
+  if (desiredPos > EncoderPos)
+  {
+    digitalWrite(retractPin, false);
+    digitalWrite(feedPin, true);
+    analogWrite(motorDriverPwmPin, currentSpeed);
+  }
+  else if (desiredPos < EncoderPos)
+  {
+    digitalWrite(retractPin, true);
+    digitalWrite(feedPin, false);
+    analogWrite(motorDriverPwmPin, currentSpeed);
+  }
+  else
+  {
+    currentSpeed = 0;
+    analogWrite(motorDriverPwmPin, currentSpeed);
     Serial.println("Correct position");
     SmState = DONE;
   }
-  EncoderLast = EncoderCurrent;
 }
 
 void Sm_Done(void)
