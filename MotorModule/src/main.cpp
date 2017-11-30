7d36c8f819de5e5ba1204da461ab31bdbbf8a236
Mauro de Lyon <maurodelyon@gmail.com>
2017-11-30 12:11:00 +0100

    input distance is now millimeter

--- MotorModule/src/main.cpp
+++ MotorModule/src/main.cpp
@@ -1,4 +1,9 @@
 #include "Arduino.h"
+#define PI 3.1415926535897932384626433832795
+#define ENCODER_DIAMETER 32
+#define TICKS 128
+#define MMPERTICK (ENCODER_DIAMETER * PI) / TICKS
+
 /*
 * State machine declaration
 */
@@ -45,8 +50,8 @@ int EncoderPinA = 7;
 int EncoderPinB = 8;
 
 // Global variables
-int EncoderPos = 0;
-int desiredPos = 0;
+double EncoderPos = 0;
+double desiredPos = 0;
 
 int desiredSpeed = 0;
 int currentSpeed = 0;
@@ -70,9 +75,9 @@ void setup()
   // encoder setup
   pinMode(EncoderPinA, INPUT);
   pinMode(EncoderPinB, INPUT);
-  
+
   //interrupt setup:
-  attachInterrupt(digitalPinToInterrupt(EncoderPinA), interruptEncoder,RISING);
+  attachInterrupt(digitalPinToInterrupt(EncoderPinA), interruptEncoder, RISING);
 
   Serial.begin(9600);
 }
@@ -91,16 +96,17 @@ void loop()
 //interrupt functions:
 void interruptEncoder()
 {
-    if (digitalRead(EncoderPinB) == LOW)
-        EncoderPos--;
-    else
-        EncoderPos++;
-    Serial.print("Current length: ");
-    Serial.print(EncoderPos);
-    Serial.print(" Desired length: ");
-    Serial.println(desiredPos);
+  if (digitalRead(EncoderPinB) == LOW)
+    EncoderPos -= MMPERTICK;
+  else
+    EncoderPos += MMPERTICK;
+  Serial.print("Current length: ");
+  Serial.print(EncoderPos);
+  Serial.print(" Desired length: ");
+  Serial.print(desiredPos);
+  Serial.print(" Current speed: ");
+  Serial.println(currentSpeed);
 }
-
 
 //state functions:
 void Sm_Listening(void)
@@ -143,56 +149,59 @@ void Sm_SettingLength(void)
   Serial.print(" Desired length: ");
   Serial.print(desiredPos);
   Serial.print(" Speed: ");
-  Serial.println(desiredSpeed);
+  Serial.println(currentSpeed);
   SmState = DONE;
 }
-
+/*
+* 
+*
+*/
 void Sm_Moving(void)
 {
   // Move rope to desired length
-  if (currentSpeed < desiredSpeed && acceleration > 500)
-
+  if (desiredPos > EncoderPos)
     direction = false;
-  }
   else
-  {
     direction = true;
-  }
-  
+
   // Speed
   if (acceleration > 10)
   {
-    //breakingzone
-    if (((EncoderPos > desiredPos - 20 && !direction )|| (EncoderPos < desiredPos + 20 && direction)) && currentSpeed > 30)
+    // Breakingzone
+    if (((EncoderPos > desiredPos - 50 && !direction) || (EncoderPos < desiredPos + 50 && direction)) && currentSpeed > 30)
     {
       currentSpeed--;
     }
+    // Accelerate
     else if (currentSpeed < desiredSpeed)
     {
-      currentSpeed++;
+      currentSpeed = desiredSpeed;
+      //currentSpeed++;
     }
     acceleration = 0;
   }
   acceleration++;
 
-  if (desiredPos == EncoderPos)
+  if ((EncoderPos < (desiredPos + MMPERTICK)) && (EncoderPos > (desiredPos - MMPERTICK)))
   {
     currentSpeed = 0;
     Serial.println("Correct position");
+    Serial.print("Desired position: ");
+    Serial.println(desiredPos);
+    Serial.print("Current position: ");
+    Serial.println(EncoderPos);
     SmState = DONE;
   }
   digitalWrite(retractPin, direction);
   digitalWrite(feedPin, !direction);
   analogWrite(motorDriverPwmPin, currentSpeed);
-
-  Serial.print("Current length: ");
-  Serial.print(EncoderPos);
-  Serial.print(" Desired length: ");
-  Serial.print(desiredPos);
-  Serial.print(" Speed: ");
-  Serial.println(currentSpeed);
 }
 
+/*
+* In this state the application should transmit a done signal.
+* The application will then wait for the main controller to respond.
+* When the acknowledge is received the state will go to listening.
+*/
 void Sm_Done(void)
 {
   // transmit done and wait for received
