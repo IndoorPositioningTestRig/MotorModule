1299e45014ae17fad51efadc45976cc49eea66d3
Mauro de Lyon <maurodelyon@gmail.com>
2018-01-09 12:33:50 +0100

    changed pins and fixed direction

--- PWMdrive/PWMdrive.ino
+++ PWMdrive/PWMdrive.ino
@@ -1,24 +1,22 @@
 // Pin declaration
-int clockWise = 4;
-int counterClockWise = 2;
+int clockWise = 7;
+int counterClockWise = 4;
 int pwmPin = 3;
 
 void Retract(int Speed) {
-  digitalWrite(clockWise, true);
-  digitalWrite(counterClockWise, !true);
-  analogWrite(pwmPin, Speed);
-}
-
-void Feed(int Speed) {
   digitalWrite(clockWise, false);
   digitalWrite(counterClockWise, !false);
   analogWrite(pwmPin, Speed);
 }
 
+void Feed(int Speed) {
+  digitalWrite(clockWise, true);
+  digitalWrite(counterClockWise, !true);
+  analogWrite(pwmPin, Speed);
+}
+
 
 void setup() {
-  Serial.begin(9600);
-
   // Set pin modes
   pinMode(pwmPin, OUTPUT);
   pinMode(clockWise, OUTPUT);
